79ddcbff27374f2b8e2849413ce7fa94ff246b25
Carlos Eduardo Magalhães <github@cadumagalhaes.dev>
2023-07-31 21:40:37 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
 output "group" {
   value = google_compute_instance_group_manager.instance_group_manager.instance_group
 }
-
-output "group_id" {
-  value = google_compute_instance_group_manager.instance_group_manager.instance_group.id
-}
-
-output "group_status" {
-  value = google_compute_instance_group_manager.instance_group_manager.instance_group.status
-}
